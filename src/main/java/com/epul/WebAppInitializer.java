33c61d88f3ceffe91baf5108deed12fc3f445a48
Laura <premillieu.laura@gmail.com>
2016-01-10 14:56:09 +0100

    CORS autorizations

--- src/main/java/com/epul/WebAppInitializer.java
+++ src/main/java/com/epul/WebAppInitializer.java
@@ -1,10 +1,12 @@
 package com.epul;
 
+import com.thetransactioncompany.cors.CORSFilter;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -24,6 +26,14 @@ public class WebAppInitializer implements WebApplicationInitializer {
             ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(appContext));
             dispatcher.setLoadOnStartup(1);
             dispatcher.addMapping("/*");
+
+
+            FilterRegistration corsFilter = servletContext.addFilter("cors", CORSFilter.class);
+            corsFilter.addMappingForUrlPatterns(null, false, "/*");
+        /*corsFilter.setInitParameter("cors.allowOrigin", "http://localhost:4991");*/
+            corsFilter.setInitParameter("cors.allowOrigin", "*");
+            corsFilter.setInitParameter("cors.supportedMethods", "OPTIONS, GET, POST, PUT, DELETE, HEAD");
+            corsFilter.setInitParameter("cors.supportedHeaders", "Accept, Content-Type");
 
             servletContext.addListener(new ContextLoaderListener(appContext));
         }
